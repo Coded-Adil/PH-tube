5ab6a2f7e08c64b959f55cb866ef5adb4c14391e
Coded-Adil <itsadil2022@gmail.com>
2024-10-07 01:20:26 +0600

    Added display categories commit

--- scripts/video.js
+++ scripts/video.js
@@ -1,14 +1,31 @@
 console.log('done')
 // fetch load catagories and show them
-const loadCatagories = () => {
-    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
-        .then((res) => res.json())
-        .then(data => displayCatagories(data.categories))
-        .catch((error) => console.log(error))
-};
+const loadCategories = () => {
+    //fetch the data
+    fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+      .then((res) => res.json())
+      .then((data) => displayCategories(data.categories))
+      .catch((error) => console.log(error))
+  };
 
-const displayCatagories = (data) => {
-    console.log(data);
-}
+  
+  const displayCategories = (categories) => {
+    const categoryContainer = document.getElementById("categories");
+  
+    categories.forEach((item) => {
+      console.log(item);
+      //create a button
+      const buttonContainer = document.createElement("div");
+      buttonContainer.innerHTML = `
+        <button id="btn-${item.category_id}" onclick="loadCategoryVideos(${item.category_id})" class="btn category-btn">
+         ${item.category}
+        </button>
+      `;
+  
+      //add button to category container
+      categoryContainer.append(buttonContainer);
+    });
+  };
+  
 
-loadCatagories()
+  loadCategories()
