eedf22d83ded0bd39dab9302cbc2c64c43b0efa4
Coded-Adil <itsadil2022@gmail.com>
2024-10-07 21:34:57 +0600

    Added conditional time commit

--- scripts/video.js
+++ scripts/video.js
@@ -53,6 +53,15 @@ const loadVideos = () => {
     "description": "'Beyond The Pale' by Jim Gaffigan, with 2.6K views, is a comedic gem that explores everyday observations and family life with a light-hearted and witty approach. Jim's humor is accessible and delightful, making this show perfect for anyone who enjoys clean, observational comedy."
 }
 
+function getTime(time) {
+  const hour = parseInt(time / 3600);
+  let remainingSec = time % 3600;
+  const min = parseInt(remainingSec / 60);
+  remainingSec = remainingSec % 60;
+
+  return `${hour} hrs ${min} min ${remainingSec} sec ago`;
+}
+
   const displayVideos = (videos) => {
     const videoContainer = document.getElementById('videos');
 
@@ -67,9 +76,10 @@ const loadVideos = () => {
           src=${video.thumbnail}
           class="h-full w-full object-cover"
           alt="Shoes" />
-        <span class="absolute right-2 bottom-2 bg-gray-700 rounded p-1 text-white">
-          ${video.others.posted_date}
-        </span>
+        ${video.others.posted_date.length == 0 ? "" : `<span class="absolute right-2 bottom-2 bg-gray-700 rounded p-1 text-white text-xs">
+          ${getTime(video.others.posted_date)}
+        </span>`}
+        
       </figure>
       <div class="px-0 py-2 flex gap-4">
         <div>
