3b83c410738589fe1d60d960800f14a9e77ce49a
Coded-Adil <itsadil2022@gmail.com>
2024-10-07 22:00:18 +0600

    Added category buttons to work commit

--- scripts/video.js
+++ scripts/video.js
@@ -15,6 +15,15 @@ const loadVideos = () => {
       .catch((error) => console.log(error))
   };
 
+const loadCategoryVideos = (id) => {
+  // alert(id);
+  // fetch
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+      .then((res) => res.json())
+      .then((data) => displayVideos(data.category))
+      .catch((error) => console.log(error))
+}
+
   
   const displayCategories = (categories) => {
     const categoryContainer = document.getElementById("categories");
@@ -64,7 +73,7 @@ function getTime(time) {
 
   const displayVideos = (videos) => {
     const videoContainer = document.getElementById('videos');
-
+    videoContainer.innerHTML="";
     videos.forEach( (video) => {
       console.log(video);
       const card = document.createElement('div');
